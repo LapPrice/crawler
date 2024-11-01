2f5515b9b2d6aaa7b4f0646dbfac574a4ff847c4
s-hwan <tmdghks507@daum.net>
2024-11-01 16:44:29 +0900

    크롤링 1차...

--- crawling.py
+++ crawling.py
@@ -1,4 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from urllib.parse import quote, unquote
 import time
 
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+
+def joongonaraGetUrl(page_number):
+    url = 'https://web.joongna.com/search/노트북?page='+str(page_number)
+    driver.get(url)
+    
+    try:
+        items = driver.find_elements(By.XPATH, "/html/body/div/div/main/div[1]/div/ul[2]/li/a")
+
+        valid_links = []
+
+        for item in items:
+            url = item.get_attribute("href")
+            if url.startswith("https://web"):  # URL이 "https://web"으로 시작하는지 확인
+                valid_links.append(url)
+        
+        set_list = set(valid_links)
+
+        for url in set_list:
+            print(url)
+
+    finally:
+        # 4. 드라이버 종료
+        time.sleep(2)
+        driver.quit()
+
+
+joongonaraGetUrl(1)
